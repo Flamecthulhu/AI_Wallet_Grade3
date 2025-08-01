bd723e46187087e9d88f20a6d7b29136fa908a0b
Allan Cosmo <86086504+Flamecthulhu@users.noreply.github.com>
2025-08-01 20:07:25 +0800

    Delete TMRT_station.py

--- TMRT_station.py
+++
@@ -1,7 +0,0 @@
-import csv
-
-with open("C:\\Users\\allan\\Programs\\Program_Grade3\\source\\TaichungMRT_Station.csv", newline='', encoding='utf-8-sig') as csvfile:
-    reader = csv.DictReader(csvfile)
-    header = next(reader)
-    for col in reader:
-        print(col)
