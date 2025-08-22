bc1d491b6007da40b6965807529db3307ba13c2c
Flamecthulhu <allancosmo107001@gmail.com>
2025-08-22 17:05:14 +0800

    [2025-08-22 17:04:50 From macOS]: Add closure/function/ifelse.plaground

---
+++ Basic/Ifelse.playground/Contents.swift
@@ -0,0 +1,26 @@
+import Foundation
+
+let myAge = 16
+let myName = "yee"
+let yourAge = 18
+let yourName = "foo"
+
+if myName == "yee"{
+    "your name is \(myName)"
+}
+else{
+    "yeee"
+}
+if myName == "yeee"{
+    "yeet"
+}
+else if myName == "yee"{
+    "yeeet"
+}
+else {
+    "sheit"
+}
+
+if myName == "yee" && myAge == 16 {
+    "bruh"
+}
