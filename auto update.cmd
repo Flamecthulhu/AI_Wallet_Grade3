159d13534917a2db9833f396f4caf646857bfe01
Flamecthulhu <allancosmo107001@gmail.com>
2025-08-23 20:24:47 +0800

    [2025-08-23 20:24:43 From Windows]: test

--- auto update.cmd
+++ auto update.cmd
@@ -1,7 +1,8 @@
 @echo off
-for /f "tokens=2 delims==" %%i in ('"wmic os get localdatetime /value"') do set ldt=%%i
-set current_date=%ldt:~0,4%-%ldt:~4,2%-%ldt:~6,2%
-set current_time=%ldt:~8,2%:%ldt:~10,2%:%ldt:~12,2%
+for /f "delims=" %%i in ('powershell -NoProfile -Command "Get-Date -Format \"yyyy-MM-dd HH:mm:ss\""') do set datetime=%%i
+
+set current_date=%datetime:~0,10%
+set current_time=%datetime:~11,8%
 
 git config --global user.name "Flamecthulhu"
 git config --global user.email "allancosmo107001@gmail.com"
@@ -11,4 +12,4 @@ git add .
 git commit -m "[%current_date% %current_time% From Windows]: %UserUpdate%"
 git push origin main
 
-REM [2025-08-23 20:10:32 From Windows]:
+REM 範例輸出: [2025-08-23 20:10:32 From Windows]:
