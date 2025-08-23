365db4c2e052e1bd64e5e90ada0d4bc4feab57d0
Flamecthulhu <allancosmo107001@gmail.com>
2025-08-23 21:06:25 +0800

    [2025-08-23 21:06:07 From Windows 11 Pro N]: auto update.cmd:Remove comments

--- auto update.cmd
+++ auto update.cmd
@@ -3,21 +3,16 @@ for /f "delims=" %%i in ('powershell -NoProfile -Command "Get-Date -Format \"yyy
 set current_date=%datetime:~0,10%
 set current_time=%datetime:~11,8%
 for /f "delims=" %%i in ('powershell -NoProfile -Command "(Get-CimInstance Win32_OperatingSystem).Caption -replace \"Microsoft \""') do set current_os=%%i
-
-REM 檢查 user.name
 for /f "delims=" %%u in ('git config --global user.name') do set gitname=%%u
 if "%gitname%"=="" (
     set /p gitname=Enter your git user name: 
     git config --global user.name "%gitname%"
 )
-
-REM 檢查 user.email
 for /f "delims=" %%e in ('git config --global user.email') do set gitemail=%%e
 if "%gitemail%"=="" (
     set /p gitemail=Enter your git user email: 
     git config --global user.email "%gitemail%"
 )
-
 set /p UserUpdate=Enter your update message: 
 git config --global --list
 git add .
