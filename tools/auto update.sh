d316771929b4175cfd5aa5b08d4e7392bad4d566
Flamecthulhu <allancosmo107001@gmail.com>
2025-08-26 09:47:36 +0800

    [2025-08-26 09:47:21 From macOS 12.7.6]: Repair auto update.sh

--- tools/auto update.sh
+++ tools/auto update.sh
@@ -1,7 +1,7 @@
 current_time=$(date +"%H:%M:%S")
 current_date=$(date +"%Y-%m-%d")
 current_os="$(sw_vers -productName) $(sw_vers -productVersion)"
-echo "$current_time $current_date $current_os"
+
 if ! git config --global user.name > /dev/null; then
     read -p "Enter your git user name: " gitname
     git config --global user.name "$gitname"
@@ -12,3 +12,8 @@ if ! git config --global user.email > /dev/null; then
     git config --global user.email "$gitemail"
 fi
 
+read -p "Enter your update message: " UserUpdate
+git config --global --list
+git add .
+git commit -m "[${current_date} ${current_time} From ${current_os}]: ${UserUpdate}"
+git push origin main
