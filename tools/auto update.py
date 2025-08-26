58fab5fd745c6cf17457602fc9cdcbf6db3e0208
Flamecthulhu <allancosmo107001@gmail.com>
2025-08-26 09:45:25 +0800

    [2025-08-26 09:44:47 From macOS 12.7.6]: git config --global --list

--- tools/auto update.py
+++ tools/auto update.py
@@ -1,14 +1,14 @@
-#!/usr/bin/env python3
 import subprocess
 import datetime
-import getpass
 
-# 取得時間與 macOS 版本
 current_time = datetime.datetime.now().strftime("%H:%M:%S")
 current_date = datetime.datetime.now().strftime("%Y-%m-%d")
 current_os = subprocess.getoutput("sw_vers -productName") + " " + subprocess.getoutput("sw_vers -productVersion")
+if current_os == "no such":
+    current_os = subprocess.getoutput('powershell -NoProfile -Command "(Get-CimInstance Win32_OperatingSystem).Caption"')
+    #powershell -NoProfile -Command "(Get-CimInstance Win32_OperatingSystem).Caption"
 
-# 設定 git 使用者名稱與 email
+
 def set_git_config(key, prompt):
     try:
         value = subprocess.check_output(["git", "config", "--global", key]).decode().strip()
@@ -18,11 +18,7 @@ def set_git_config(key, prompt):
 
 set_git_config("user.name", "Enter your git user name: ")
 set_git_config("user.email", "Enter your git user email: ")
-
-# 輸入更新訊息
 user_update = input("Enter your update message: ")
-
-# 執行 git 指令
 subprocess.run(["git", "add", "."])
 commit_msg = f"[{current_date} {current_time} From {current_os}]: {user_update}"
 subprocess.run(["git", "commit", "-m", commit_msg])
